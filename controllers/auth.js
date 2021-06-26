970ad5fda1f348d1102eb2d00c8421b1de7cb0f1
Luis Salvador <luis.salvador@coppel.com>
2021-06-26 15:00:13 -0600

    Seccion 12 - Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const {response} = require('express');
 const Usuario = require('../models/usuario');
 const bcrypt  = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async (req, res = response) => {
@@ -48,10 +49,61 @@ const login = async (req, res = response) => {
 
 
 
+const googleSignIn = async (req, res = response) => {
+
+    const googleToken = req.body.token;
+
+    try {
+
+        const {name, email, picture} = await googleVerify(googleToken);
+
+        const usuarioDB = await Usuario.findOne({email});
+        let usuario;
+
+        if( !usuarioDB ){
+            //Si no existe el usuario
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        } else {
+            //Existe el usuario
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        // Guardar en base de datos
+        await usuario.save();
+
+        // Generar el TOKEN - JWT
+        const token = await generarJWT( usuario.id );
+
+
+        res.json({
+            ok: true,
+            msg: 'Google SignIn',
+            token
+        });
+        
+    } catch (error) {
+        res.status(401).json({
+            ok: true,
+            msg: 'Token no es correcto'
+        });
+    }
+
+
+}
+
+
 
 
 
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
