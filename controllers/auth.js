fb8338cff0137c9875ba6deeefcf032ea7b5d3c3
Luis Salvador <luis.salvador@coppel.com>
2021-06-29 11:46:56 -0600

    Usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -105,9 +105,13 @@ const renewToken = async (req, res = response) => {
     // Geneerar el TOKEN - JWT
     const token = await generarJWT(uid);
 
+    //Obtener el usuario por uid
+    const usuario = await Usuario.findById(uid)
+
     res.json({
         ok:true,
-        token
+        token,
+        usuario
     })
 
 }
